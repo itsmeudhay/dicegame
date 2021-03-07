02b508909d46e2c0ba22fe79b5bf723c3518c329
Udhayakumar Govindarajan <77959873+itsmeudhay@users.noreply.github.com>
2021-03-07 23:06:41 +0530

    add dicegame files

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var image1 = "images/dice"+ randomNumber1 +".png";
+var image2 = "images/dice"+ randomNumber2 +".png";
+
+document.querySelector(".dice .img1").setAttribute("src",image1);
+document.querySelector(".dice .img2").setAttribute("src",image2);
+
+if(randomNumber1 === randomNumber2){
+    document.querySelector(".container h1").innerHTML = "Draw"
+}
+else if(randomNumber1 > randomNumber2){
+    document.querySelector(".container h1").innerHTML = "Player 1 Win🏆"
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector(".container h1").innerHTML = "Player 2 Win🏆"
+}
